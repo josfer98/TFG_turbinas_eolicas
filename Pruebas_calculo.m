008cb2df4e9c31d3c13919e8722cdea8c1490235
josfer98 <99836209+josfer98@users.noreply.github.com>
2022-04-01 19:07:49 +0200

    Cambios varios y problemas con potencia_1

--- Pruebas_calculo.m
+++ Pruebas_calculo.m
@@ -1,80 +1,159 @@
 % José Antonio Fernández López
 % TFG - Generación de energía con una turbina eólica flotante
 
-%% Pruebas de programación
+%% Pruebas
 
 %% Configuración
-% Constantes 
-    % Densidad del aire
-        Ro = 1.225; %Kg/m^3
 
-% Variables
+    % Densidad del aire
+       Ro = 1.225; %Kg/m^3
+
     % Longitud de la pala
-        L = 5; %m
+        L = 55; %m
+
     % Número de segmentos en los que se divide la pala
         N = 5;
+        i = 1:N;
+
     % Ángulo inicial de giro
-        %Theta_1 = 5; %Grados [º]
-        Theta_1 = (5 * pi) / 180; %Rad
+        theta_1 = 3; %Grados [º]
+
     % Variación de Theta_i
-        %Delta_theta = 10; %Grados [º]
-        Delta_theta = (10 * pi) / 180; %Rad
+        Delta_theta = 0.05; %Grados [º]
+
+        %Creamos el ángulo de torsión
+        theta_i = zeros(1,N);
+    
+            for j = 1:5
+                if j < 2
+                    theta_i(1) = theta_1;
+                else
+                    theta_i(j) = theta_i(j-1) + Delta_theta;
+                end
+            end
+    
+        theta_1 = (theta_1 * pi) / 180; %Rad
+        Delta_theta = (Delta_theta * pi) / 180; %Rad
+        theta_i = (theta_i .* pi) / 180; %Rad
+        
     % Velocidad del viento
-        u = [5 8 10 12]; %m/s
-    % Chord line, de momento sin _i
-        c = [2]; %m
-    % Ángulo de reducción de la Chord line a lo largo de la pala de la
-    % turbina eólica
-        %Phi = 20; Grados [º]
-        Phi = (20 * pi) / 180; %Rad
+        u = 1:1:100; %m/s
 
-
-    % Tiempo, se puede usar como algo concreto o como array para
-    % representaciones
-        tiempo = 0; % ¿Qué debemos asignarle?
+    % Densidad del material de la pala
+        CFRP = 18000; %kg/m^3
+        GFRP = 15000; %kg/m^3
+        GFEpoxi = 17000; %kg/m^3
+        dens_pala = [CFRP GFRP GFEpoxi];
+    
+    % Masa de la pala
+        masa_pala = 5000; %Kg
 
 %% Fórmulas para el cálculo inicial y completo de la pala de la turbina eólica.
+ 
+  % Establecemos el buje y la punta
+    Buje = 3; %m
+    Punta = 1; %m
+    L_i = L/N;
+  % Se calcula la hipotenusa de borde de fuga
+    H_bf = sqrt(((Buje - Punta)^2) + L^2);
+  % Ahora el ángulo Phi, con el que decrece la chord line a lo largo de L
+    Phi = asin( (Buje - Punta) / H_bf );
+    Phi_deg = (Phi * 180) / pi
+  % Variables necesarias para el cálculo de la chord line
+    altura_i = (((2*i) -1) * L) / (2*N);
+    diagonal_i = (((2*i) -1) * H_bf) / (2*N);
+    x_i = sqrt(diagonal_i.^2 - altura_i.^2);
+   % Ya se puede obtener la línea de cuerda de cada segmento
+    c_i = Buje - x_i;
+    
   % Usando algunas fórmulas del desarrollo de Carlos Armenta Deu,
   % referenciado en mi trabajo.
     % Lado inicial de la pala
-        c_left_pala = c + (L/2) * tan(Phi);
+        c_left_i = c_i + (L_i/2) * tan(Phi);
     % Lado final de la pala
-        c_right_pala = c - (L/2) * tan(Phi);
+        c_right_i = c_i - (L_i/2) * tan(Phi);
     % Área de la pala
-        a = ((c_left_pala + c_right_pala) * L) / 2;
-    
-    % Fuerza del viento
-        F_viento = (1/2) * Ro * a * u.^2;
-    % Fuerza normal
-        F_normal = F_viento * sin(Theta_1);
-    % Brazo := distancia entre el inicio de la pala y el centro de giro.
-        %En este caso voy a suponer un valor hasta conocer como se debe
-        %calcular, el valor debe ser aprox ~0.5-0.6 la pala
-        brazo_inicial = 0.55 * L;
-    % Momento de torsión
-        torque_0 = F_normal * brazo_inicial
-    
+        S_i = ((c_left_i + c_right_i) * L) / 2;
+
+    %Definición del brazo
+        cateto_buje = (Buje/2) - (Punta/2);
+        R_brazo = sqrt(cateto_buje.^2 + L.^2);
+        brazo_i = (((2*i) -1) * R_brazo) / (2*N);
+
+
+        %EL PROBLEMA TIENE QUE ESTAR AQUÍ
+    % Momento inercia del área de un trapecio
+        I_area = (L_i^3).*((c_right_i.^2) + (4.*c_right_i.*c_left_i) + (c_left_i.^2)) ./ (36 .* (c_right_i + c_left_i))
+
     % Momento de inercia
-        % I = masa_pala * (brazo_inicial^2)
+        I = dens_pala(1) .* I_area
+
+    % Momento de inercia de un rectángulo
+        S_pala = sum(S_i); 
+        m_i = (S_i/S_pala) * masa_pala;
+
+    % Calculamos el tiempo, lo que tarda el viento en atravesar el
+    % segmento
+        intervalo_tiempo = c_i ./ u(20);
+    
+%% Cuando solo presenta ángulo de cabeceo
+    % Fuerza del viento
+        F_viento_i = (1/2) .* Ro .* S_i .* u(20).^2
+    % Fuerza normal
+        F_normal_i = F_viento_i .* sin(theta_1)
+    % Momento de torsión
+        torque_0 = F_normal_i .* brazo_i;
+    % Torque global
+        torque_global_0 = sum(torque_0)
+
+
+
     % Aceleración angular
-        % alpha_ang = torque_0 / I;
+        alpha_ang_0 = torque_global_0 ./ I;
     % Velocidad angular
-        % Omega = alpha_ang * tiempo;
-    
+        Omega_0 = alpha_ang_0 .* intervalo_tiempo;
     % Potencia de la pala
-        % potencia_0 = torque_0 * Omega;
+        potencia_0 = torque_global_0 * Omega_0;
+%% Cuando presenta ángulo de cabeceo y torsión de los segmentos
+
+    % Fuerza normal
+        F_normal_i_torsion = F_viento_i .* sin(theta_i)
+    % Momento de torsión
+        %Se necesita multiplicar por el seno del ángulo de torsión para
+        %sacar el torque del área efectiva
+        torque_1 = zeros(1,N);
+      
+            for j = 1:5
+                if j < 2
+                    torque_1(1) = F_normal_i_torsion(1) .* brazo_i(1);
+                else
+                       %AQUÍ TMB SE ME PRESENTA UN PROBLEMA, SE ME HACE LA
+                       %MEDIDA MUY PEQUEÑA, ALGO PASA.
+                    torque_1(j) = F_normal_i_torsion(j) .* brazo_i(j) ./ sin(Delta_theta);
+                end
+            end
+
+    % Torque global
+        torque_global_1 = sum(torque_1)
+
+    % Aceleración angular
+        alpha_ang_1 = torque_global_1 ./ I
+    % Velocidad angular
+        Omega_1 = alpha_ang_1 .* intervalo_tiempo
+    % Potencia de la pala
+        potencia_1 = torque_global_1 * Omega_1
 
 
-%% Representaciones
-    
+        
+ %% Representaciones
 
-    %Desconozco si esta es una correcta representación, en uno de los
-    %estudios se representaba con xlabel de tip speed.
-    
-    figure('Name','Potencia vs. Velocidad del viento','NumberTitle','off');
-        plot(u,potencia_0);
-        xlabel('Velocidad del viento (m/s)');
-        ylabel('Potencia de la pala (W)');
+    figure('Name','Potencia en cada segmento de la pala con u fija','NumberTitle','off');
+        plot(intervalo_tiempo,potencia_1);
+        hold on;
+        plot(intervalo_tiempo,potencia_0);
+        xlabel('Intervalo de tiempo (s)');
+        ylabel('Potencia (W)');
+        legend('Potencia de torsión','Potencia básica')
 
 
 
